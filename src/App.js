bdbbbd20b808860cdfaf60756a76b72772aee714
sonalichaudhary844@gmail.com <sonalichaudhary884>
2024-09-10 13:08:55 +0530

    product.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import "./App.css";
+
+import Title from"./Title.js";
+import Product from "./Product.js";
+
+
+function App() {
+  return(
+    <>
+      <Product/>
+      <Product/>
+      <Product/>
+      
+    </>
+  
+  );
+
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+function Title( ){
+    return (
+    <div>
+        <h1>I am the title!</h1>
+    <h3>I am the Description</h3>
+    </div>
+    );
+  }
+  export default Title;
+  
